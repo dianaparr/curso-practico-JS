5cc3847e0953246514584b723b2989b315a506a9
dianaparr <dianaparra.9611@gmail.com>
2021-12-18 22:57:04 -0500

    add function to discover the moda

--- moda.js
+++ moda.js
@@ -1,33 +1,23 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1
-];
+function moda(arr) {
+    const lista1Count = {};
 
-const lista1Count = {};
+    arr.map(
+        function (elemento) {
+            if (lista1Count[elemento]) {
+                lista1Count[elemento] += 1; //sumarle 1 a cada una de las posiciones dentro de los objetos cuando se repiten
+            } else {
+                lista1Count[elemento] = 1;
+            };
+        }
+    );
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1; //sumarle 1 a cada una de las posiciones dentro de los objetos cuando se repiten
-        } else {
-            lista1Count[elemento] = 1;
-        };
-    }
-);
+    const lista1Array = Object.entries(lista1Count).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];
+        }
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1] - elementoB[1];
-    }
-);
+    const moda = lista1Array[lista1Array.length - 1];
 
-const moda = lista1Array[lista1Array.length - 1];
+    return moda;
+};
